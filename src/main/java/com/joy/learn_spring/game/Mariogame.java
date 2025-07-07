1d20c9a6cabe786d8fb7f079e66abdcd13466440
Shuvodeep Talukdar <iamst991@gmail.com>
2025-07-07 22:28:17 +0530

    added controller annotations to the game classes and fixed the run method in LearnSpringApplication.java

---
+++ src/main/java/com/joy/learn_spring/game/Mariogame.java
@@ -0,0 +1,21 @@
+package com.joy.learn_spring.game;
+
+public class Mariogame {
+
+
+    public void up() {
+        System.out.println("Jump");
+    }
+
+    public void down() {
+        System.out.println("Slide");
+    }
+
+    public void left() {
+        System.out.println("Go back");
+    }
+
+    public void right() {
+        System.out.println("Accelerate");
+    }
+}
